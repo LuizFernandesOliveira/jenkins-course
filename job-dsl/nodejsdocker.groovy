92f117a0505ad43db66eeb56804390f0ce7f6133
Luiz Fernandes de Oliveira <36059306+LuizFernandesOliveira@users.noreply.github.com>
2022-10-25 22:01:31 -0300

    upgrade

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/LuizFernandesOliveira/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('LuizFernandesOliveira')
+            node / gitConfigEmail('luizfernandesoliveiraoficial@gmail.com')
         }
     }
     triggers {
@@ -14,9 +14,9 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('lfooficial/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('docker')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
